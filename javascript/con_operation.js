34c501cada80264fcecfd436af6537aa8faacb3f
Raj <86738087+raj9713@users.noreply.github.com>
2024-02-06 12:06:56 +0000

    convarsin done

---
+++ javascript/con_operation.js
@@ -0,0 +1,24 @@
+let score="40"
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+//"40"=>40
+//"40ad"= NAN
+//true =>1
+
+let isLogged = 1
+let booleanisloggedin = Boolean(isLogged)
+//1 ->true 0->false
+// "" ->false
+// "dfghj"->true
+let somenumber=33
+let strinignumber = String(somenumber)
+console.log(strinignumber);
+console.log(typeof strinignumber);
---
+++ javascript/variables.js
@@ -0,0 +1,11 @@
+const accountId = 100
+let accountEmai = "pannarajkumar1996@gmail.com"
+var accountNumber = "123456"
+accountCity ="panna"
+let ac;
+// accountId = 2 not allowed
+
+
+console.log(accountId)
+
+console.table([accountEmai,accountId,accountCity,accountNumber,ac])
