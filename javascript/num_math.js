9a78c01de235906a5e16c1f5ec174afc0c138d56
Raj <86738087+raj9713@users.noreply.github.com>
2024-02-07 10:57:12 +0000

    done

---
+++ javascript/num_math.js
@@ -0,0 +1,37 @@
+const score = 400
+
+console.log(score);
+const blance1 = new Number(200)
+console.log(blance1);
+
+
+
+const blance = new Number('jbsnd')
+console.log(blance);
+
+console.log(blance.toString().length);
+console.log(blance1.toFixed(2))
+const othenumber = 23.5444
+console.log(othenumber.toPrecision());
+const newnum =1000000
+console.log(newnum.toLocaleString());
+console.log(newnum.toLocaleString('en-IN'));
+
+// maths------------------------------------------------
+console.log(Math);
+console.log(Math.abs(-4))
+
+console.log(Math.round(4.4))
+console.log(Math.floor(4.9))
+console.log(Math.ceil(4.6))
+
+
+
+
+console.log((Math.random()*10) +1)
+console.log(Math.floor(Math.random()*10) +1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() *(max - min + 1)) + min)
+
