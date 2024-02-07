bc55fa00bba8ac09c98305efeda9b1b07964c50e
Raj <86738087+raj9713@users.noreply.github.com>
2024-02-07 12:48:54 +0000

    done

---
+++ javascript/date_time.js
@@ -0,0 +1,30 @@
+let mydate = new Date()
+console.log(mydate);
+console.log(mydate.toString);
+console.log(mydate.toLocaleString);
+console.log(mydate.toDateString);
+console.log(typeof mydate )
+
+let createdate = new Date (2023 , 23)
+let createdate1 = new Date (2023 , 1, 23)
+console.log(createdate.toDateString());
+console.log(createdate1.toDateString());
+   
+
+let datetime = new Date("02-07-2024")
+let mytime = Date.now()
+// console.log(mytime)
+// console.log(datetime.getTime());
+// second nikale ke liye
+
+let mytime1 = Date.now()
+
+console.log(Math.floor(Date.now()/1000));
+
+
+
+
+
+
+
+
