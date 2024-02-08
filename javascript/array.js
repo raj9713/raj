5b833f12f42959afb8c4df79c9283f4b7d14382b
Raj <86738087+raj9713@users.noreply.github.com>
2024-02-08 06:35:24 +0000

    done

---
+++ javascript/array.js
@@ -0,0 +1,26 @@
+const myarr =[1,2,3,4,5,6,7,8,9]
+
+ const newarray = myarr.join()
+
+console.log ( typeof myarr);
+console.log(typeof newarray);
+//slice ,splice
+
+console.log("A " , myarr);
+const nw1 = myarr.slice(1, 3)
+console.log(nw1);
+
+console.log("B ", myarr);
+const nw2 = myarr.splice(1, 3)
+console.log(myarr);
+console.log(nw2);
+
+const stringarray = ["raj","vasu","ad","jvs"]
+const stringarray1 =["a","b","c","d"]
+// stringarray.push(stringarray1)
+// console.log(stringarray);
+
+// const raj =stringarray.concat(stringarray1)
+// console.log(raj); 
+ 
+const raj1 =[...stringarray , ...stringarray1]
